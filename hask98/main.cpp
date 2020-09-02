4e16ba8ff8445e5b382011fa82f26cdb72b33ecf
Siddharth Bhat <siddu.druid@gmail.com>
2020-09-03 01:35:31 +0530

    [SIDEQUEST] Fuck this, let's just reboot hask98 from scratch on the weekend

---
+++ hask98/main.cpp
@@ -0,0 +1,6 @@
+// Haskell 98 parser that builds an AST of Hask98 sources.
+// Read the Haskell98 report here: https://www.haskell.org/definition/haskell98-report.pdf
+  
+int main() {
+    return 0;
+}
