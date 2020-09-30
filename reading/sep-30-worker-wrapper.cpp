47dc74af43a3005d685a902404f8b99282ccce09
Siddharth Bhat <siddu.druid@gmail.com>
2020-09-30 18:00:53 +0530

    [WIP] filling out transformations

--- reading/sep-30-worker-wrapper.cpp
+++ reading/sep-30-worker-wrapper.cpp
@@ -38,35 +38,11 @@ struct Thunk {
 private:
   std::optional<T> cached;
   std::function<T()> lzf;
-  // operator Force<T>() {
-  //   return Force<T>(lzf());
-  // };
-
-  // template<typename Inner>
-  // operator Unwrap<Force<T>, Inner> () {
-  //   return Unwrap<Force<T>, Inner>(lzf().v);
-  // };
 };
 
-template<typename T>
-struct Force {
-  T v;
-  Force(T v): v(v) {};
-  Force(Thunk<T> thnk) : v(thnk.v()) {
-  };
-  operator T() { return T(v);}
-
-};
-
-
-// template<typename T>
-// Force<T>::Force (Thunk<T> thnk) : v (thnk.lzf()) {}
 
 template<typename T>
 T force(Thunk<T> thnk) { return thnk.v(); }
-
-template<typename T>
-T force(Force<T> forcedv) { return forcedv.v; }
 
 template<typename T>
 Thunk<T> thunkify(T v) { return Thunk<T>([v]() { return v; }); }
@@ -109,9 +85,26 @@ namespace f0 {
   }
 
   int main() {
-    printf("mainf0: %d\n", f(thunkify(SimpleInt(1))).v);
+    g_count = 0;
+    printf("\n===mainf0===\n");
+    SimpleInt out = f(thunkify(SimpleInt(3)));
+    printf("out: %d\n", out.v);
   }
-}
+
+  }
+
+template<typename T>
+struct Force {
+  T v;
+  Force(T v): v(v) {};
+  Force(Thunk<T> thnk) : v(thnk.v()) {};
+  operator T() { return T(v);}
+
+};
+
+template<typename T>
+T force(Force<T> forcedv) { return forcedv.v; }
+
 
 namespace f1{
   SimpleInt f(Force<SimpleInt> i) {
@@ -130,7 +123,9 @@ namespace f1{
   }
 
   int main() {
-    printf("mainf1: %d\n", f(thunkify(SimpleInt(1))).v);
+    printf("\n===mainf1===\n");
+    SimpleInt out = f(thunkify(SimpleInt(3)));
+    printf("out: %d\n", out.v);
   }
 }
 
@@ -151,7 +146,53 @@ namespace f2{
   }
 
   int main() {
-    printf("mainf2: %d\n", f(thunkify(SimpleInt(1))).v);
+    printf("\n===mainf2===\n");
+    SimpleInt out = f(thunkify(SimpleInt(3)));
+    printf("out: %d\n", out.v);
+  }
+}
+
+template<typename Outer, typename Inner>
+struct Unwrap {
+  Inner v;
+  Unwrap(Outer outer) : v(outer) {};
+  Unwrap(Inner inner) : v(inner) {};
+  operator Inner() { return v; }
+};
+
+// specialize to allow implicit construction of a
+// Unwrap<Force<Outer>> from an Outer
+template<typename Outer, typename Inner>
+struct Unwrap<Force<Outer>, Inner> {
+  Inner v;
+  Unwrap(Outer outer) : v(outer.v) {};
+  Unwrap(Thunk<Outer> outer) : v(outer.v()) {};
+  Unwrap(Inner inner) : v(inner) {};
+  operator Inner() { return v; }
+};
+
+namespace f3{
+  SimpleInt f(Unwrap<Force<SimpleInt>, int> i) {
+    // SimpleInt icons = force(i);
+    // SimpleInt icons = i;
+    // int ihash = casedefault(icons);
+    int ihash = i;
+    if (ihash <= 0) {
+      return SimpleInt(42);
+    } else {
+      int prev = ihash - 1;
+      //SimpleInt siprev = SimpleInt(prev);
+      // Thunk<SimpleInt> siprev_t = thunkify(siprev);
+      // SimpleInt f_prev_v = apStrict(f, siprev);
+      SimpleInt f_prev_v = apStrict(f, prev);
+      return f_prev_v;
+    }
+  }
+
+  int main() {
+    printf("\n===mainf3===\n");
+    SimpleInt out = f(thunkify(SimpleInt(3)));
+    printf("out: %d\n", out.v);
   }
 }
 
@@ -232,16 +273,14 @@ namespace g2{
   }
 }
 
-namespace g3 {
-  SimpleInt g(Force<SimpleInt> i) {
+namespace g3{
+  SimpleInt g(Unwrap<Force<SimpleInt>, int> i) {
     // SimpleInt icons = force(i);
-    SimpleInt icons = i;
-    int ihash = casedefault(icons);
-
-    int retval;
+    // SimpleInt icons = i;
+    // int ihash = casedefault(icons);
+    int ihash = i;
     if (ihash <= 0) {
-      // return SimpleInt(42);
-      retval = 42;
+      return SimpleInt(42);
     } else {
       int prev = ihash - 1;
       SimpleInt siprev = SimpleInt(prev);
@@ -249,11 +288,9 @@ namespace g3 {
       SimpleInt g_prev_v = apStrict(g, siprev);
       int g_prev_v_hash = casedefault(g_prev_v);
       int rethash = g_prev_v_hash + 2;
-      // SimpleInt ret = SimpleInt(rethash);
-      // return ret;
-      retval = rethash;
+      SimpleInt ret = SimpleInt(rethash);
+      return ret;
     }
-    return SimpleInt(retval);
   }
 
   int main() {
@@ -261,55 +298,27 @@ namespace g3 {
     printf("\n===maing3===\n");
     printf("out: %d\n", g(thunkify(SimpleInt(3))).v);
   }
-};
+}
 
-
-// ===g4====
-
-// Declare that we will unwrap the SimpleInt to produce an `int`.
-template<typename Outer, typename Inner>
-struct Unwrap {
-  Inner v;
-  Unwrap(Outer outer) : v(outer) {};
-  Unwrap(Inner inner) : v(inner) {};
-  operator Inner() { return v; }
-};
-
-// specialize to allow implicit construction of a
-// Unwrap<Force<Outer>> from an Outer
-template<typename Outer, typename Inner>
-struct Unwrap<Force<Outer>, Inner> {
-  Inner v;
-  Unwrap(Outer outer) : v(outer.v) {};
-  Unwrap(Thunk<Outer> outer) : v(outer.v()) {};
-  Unwrap(Inner inner) : v(inner) {};
-  operator Inner() { return v; }
-};
-
-
-namespace g4 {
+namespace g4{
   SimpleInt g(Unwrap<Force<SimpleInt>, int> i) {
     // SimpleInt icons = force(i);
     // SimpleInt icons = i;
     // int ihash = casedefault(icons);
     int ihash = i;
-
-    int retval;
     if (ihash <= 0) {
-      // return SimpleInt(42);
-      retval = 42;
+      return SimpleInt(42);
     } else {
       int prev = ihash - 1;
-      SimpleInt siprev = SimpleInt(prev);
+      // SimpleInt siprev = SimpleInt(prev);
       // Thunk<SimpleInt> siprev_t = thunkify(siprev);
-      SimpleInt g_prev_v = apStrict(g, siprev);
+      // SimpleInt g_prev_v = apStrict(g, siprev);
+      SimpleInt g_prev_v = apStrict(g, prev);
       int g_prev_v_hash = casedefault(g_prev_v);
       int rethash = g_prev_v_hash + 2;
-      // SimpleInt ret = SimpleInt(rethash);
-      // return ret;
-      retval = rethash;
+      SimpleInt ret = SimpleInt(rethash);
+      return ret;
     }
-    return SimpleInt(retval);
   }
 
   int main() {
@@ -317,32 +326,33 @@ namespace g4 {
     printf("\n===maing4===\n");
     printf("out: %d\n", g(thunkify(SimpleInt(3))).v);
   }
-};
+}
 
-// convert ~apStrict(g, SimpleInt(prev))~ into ~apStrict(g, prev)~
-namespace g5 {
+namespace g5{
   SimpleInt g(Unwrap<Force<SimpleInt>, int> i) {
+    std::optional<int> iret; // return value
+
     // SimpleInt icons = force(i);
     // SimpleInt icons = i;
     // int ihash = casedefault(icons);
     int ihash = i;
-
-    int retval;
     if (ihash <= 0) {
-      // return SimpleInt(42);
-      retval = 42;
+      // return 42
+      iret = 42;
     } else {
       int prev = ihash - 1;
       // SimpleInt siprev = SimpleInt(prev);
       // Thunk<SimpleInt> siprev_t = thunkify(siprev);
+      // SimpleInt g_prev_v = apStrict(g, siprev);
       SimpleInt g_prev_v = apStrict(g, prev);
       int g_prev_v_hash = casedefault(g_prev_v);
       int rethash = g_prev_v_hash + 2;
       // SimpleInt ret = SimpleInt(rethash);
       // return ret;
-      retval = rethash;
+      iret = rethash;
     }
-    return SimpleInt(retval);
+    assert(iret);
+    return SimpleInt(*iret);
   }
 
   int main() {
@@ -350,109 +360,17 @@ namespace g5 {
     printf("\n===maing5===\n");
     printf("out: %d\n", g(thunkify(SimpleInt(3))).v);
   }
-};
-
-// ===g6===
-
-// denotes that we are wrapping a value into another value.
-template <typename Inner, typename Outer>
-struct Wrap {
-  Inner wv;
-
-  Wrap(Inner v) : wv(v) {}
-  Wrap(Outer v) : wv(v.v) {
-    printf("unwrapping (%d)\n", ++g_count);
-  }
-
-  operator Outer () { return Outer(wv);}
-  operator Inner () {return wv; }
-};
-
-namespace g6 {
-  Wrap<int, SimpleInt> g(Unwrap<Force<SimpleInt>, int> i) {
-    // SimpleInt icons = force(i);
-    // SimpleInt icons = i;
-    // int ihash = casedefault(icons);
-    int ihash = i;
-
-    int retval;
-    if (ihash <= 0) {
-      // return SimpleInt(42);
-      retval = 42;
-    } else {
-      int prev = ihash - 1;
-      // SimpleInt siprev = SimpleInt(prev);
-      // Thunk<SimpleInt> siprev_t = thunkify(siprev);
-      SimpleInt g_prev_v = apStrict(g, prev);
-      int g_prev_v_hash = casedefault(g_prev_v);
-      int rethash = g_prev_v_hash + 2;
-      // SimpleInt ret = SimpleInt(rethash);
-      // return ret;
-      retval = rethash;
-    }
-    return retval;
-  }
-
-  int main() {
-    g_count = 0;
-    printf("\n===maing7===\n");
-    SimpleInt out = g(thunkify(SimpleInt(3)));
-    printf("out: %d\n", out.v);
-    printf("\n===maing7===\n");
-  }
-};
-
-// ===g7===
-
-// denotes that we are wrapping a value into another value.
-
-// Eliminate the unwrap to `SimpleInt` by calling `casedefault`.
-namespace g7 {
-  Wrap<int, SimpleInt> g(Unwrap<Force<SimpleInt>, int> i) {
-    // SimpleInt icons = force(i);
-    // SimpleInt icons = i;
-    // int ihash = casedefault(icons);
-    int ihash = i;
-
-    int retval;
-    if (ihash <= 0) {
-      // return SimpleInt(42);
-      retval = 42;
-    } else {
-      int prev = ihash - 1;
-      // SimpleInt siprev = SimpleInt(prev);
-      // Thunk<SimpleInt> siprev_t = thunkify(siprev);
-      // SimpleInt g_prev_v = apStrict(g, prev);
-      // int g_prev_v_hash = casedefault(g_prev_v);
-
-      int g_prev_v_hash = apStrict(g, prev);
-      int rethash = g_prev_v_hash + 2;
-      // SimpleInt ret = SimpleInt(rethash);
-      // return ret;
-      retval = rethash;
-    }
-    return retval;
-  }
-
-  int main() {
-    g_count = 0;
-    printf("\n===maing7===\n");
-    SimpleInt out = g(thunkify(SimpleInt(3)));
-    printf("out: %d\n", out.v);
-    printf("\n===maing7===\n");
-  }
-};
+}
 
 int main() {
   f0::main();
   f1::main();
   f2::main();
+  f3::main();
   g0::main();
   g1::main();
   g2::main();
   g3::main();
   g4::main();
   g5::main();
-  g6::main();
-  g7::main();
 }
